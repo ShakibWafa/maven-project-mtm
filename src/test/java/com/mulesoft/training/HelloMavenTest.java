73b178e5b8c5a49ccaad0c13cc1882905bffe745
Shakib Wafa <shakibwa@datacom.co.nz>
2016-08-22 15:49:02 +1200

    init

---
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -0,0 +1,19 @@
+package com.mulesoft.training;
+
+import org.junit.Test;
+import org.mule.tck.junit4.FunctionalTestCase;
+import static org.junit.Assert.*;
+
+public class HelloMavenTest extends FunctionalTestCase {
+
+    @Test
+    public void mavenFlowReturnsHelloMaven() throws Exception {
+        runFlowAndExpect("mavenFlow", "Hello Maven");
+    }
+    
+    @Override
+    protected String getConfigFile() {
+        return "maven-project.xml";
+    }
+
+}
